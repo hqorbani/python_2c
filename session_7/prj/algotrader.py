a27cc9d71b5838b929ef667f46e7763f72cb33e9
Hamzeh <h.qorbani7@gmail.com>
2023-04-19 09:33:52 +0330

    session_8: we worked on session_7

--- session_7/prj/algotrader.py
+++ session_7/prj/algotrader.py
@@ -11,6 +11,7 @@ class algotrader():
             return "failed to connect at account",mt5.last_error()
     def get_asset(self , asset_name , tm , candle_numbers):
         rates = mt5.copy_rates_from_pos(asset_name , tm , 0 , candle_numbers)
+        
         if rates is None:
             print(mt5.last_error())
             quit()
@@ -18,4 +19,17 @@ class algotrader():
             rates = pd.DataFrame(rates)
             rates['time'] = pd.to_datetime(rates['time'], unit = 's')
             return rates
+    def do_proccess(self,asset_name , tm , candle_numbers):
+        rates = self.get_asset(asset_name , tm , candle_numbers)
+        # print(rates)
+        last_open = rates.iloc[-1]["open"]
+        ema_28 = rates.iloc[-1]["ema_28"]
+        ema_8 = rates.iloc[-1]["ema_8"]
+        print("last_open: ",last_open)
+        if ema_8 > ema_28:
+            pass
+        #buy
+        else:
+            pass
+
 
--- session_7/prj/main.py
+++ session_7/prj/main.py
@@ -1,11 +1,17 @@
 from algotrader import algotrader
-
-server_name = "MetaQuotes-Demo"
-account = 68263282
-
+import json
+import schedule
+import time
+# import pandas as pd
+data = open("json/properties.json")
+properties = json.load(data)
 p1 = algotrader()
-if p1.connect(account , server_name):
-    reates = p1.get_asset('EURUSD' , 1 , 10)
-    print(reates)
+if p1.connect(properties["user_name"] , properties["server_name"]):
+    # reates = p1.get_asset(properties["asset"] , properties["time_frame"] , properties["candle_numbers"])
+    schedule.every(properties["time_period"]).seconds.do(p1.do_proccess, properties["asset"] , properties["time_frame"] , properties["candle_numbers"])
+    # print(reates)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 else:
     print("no connection...")
