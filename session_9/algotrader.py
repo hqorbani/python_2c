8057f5494a050f1edc3f0e4d0a75fd704c52b301
Hamzeh <h.qorbani7@gmail.com>
2023-04-27 10:54:11 +0430

    create cci and talib_doc link

--- session_9/algotrader.py
+++ session_9/algotrader.py
@@ -21,14 +21,19 @@ class algotrader():
             rates['time'] = pd.to_datetime(rates['time'], unit = 's')
             rates['ema_8'] = ta.EMA(rates['close'] , timeperiod=8)
             rates['ema_28'] = ta.EMA(rates['close'] , timeperiod=28)
+            rates['cci_30'] = ta.CCI(rates['high'], rates['low'], rates['close'], timeperiod=30)
             return rates
     def do_proccess(self,asset_name , tm , candle_numbers):
         rates = self.get_asset(asset_name , tm , candle_numbers)
         print(rates)
         # last_open = rates.iloc[-1]["open"]
         # last_high = rates.iloc[-1]["high"]
-        # ema_28 = rates.iloc[-1]["ema_28"]
-        # ema_8 = rates.iloc[-1]["ema_8"]
+        ema_28_1 = rates.iloc[-1]["ema_28"]
+        ema_8_1 = rates.iloc[-1]["ema_8"]
+        ema_28_2 = rates.iloc[-2]["ema_28"]
+        ema_8_2 = rates.iloc[-2]["ema_8"]
+        cci_30_1 = rates.iloc[-1]["cci_30"]
+        
         # print("ema_8: ",ema_8,"---","ema_28: ",ema_28)
         # if ema_8 > ema_28:
         #     print("buy")
